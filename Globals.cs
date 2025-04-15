ad6038b6cefa240a09aeb4f933990707cdd57156
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-04-15 16:14:30 -0600

    Init

---
+++ Globals.cs
@@ -0,0 +1,16 @@
+global using System;
+global using System.Data;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Threading.Tasks;
+global using Dapper;
+global using CodeWorks.Utils;
+
+// APPLICATION SPECIFIC
+global using project32_chore.Repositories;
+global using project32_chore.Services;
+global using project32_chore.Models;
---
+++ Models/Account.cs
@@ -0,0 +1,9 @@
+namespace project32_chore.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
